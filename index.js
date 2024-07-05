bcddae877b9c264c39f297a3be1b451625a52515
sandy-sandesh <sandeshtimalsina71@gmail.com>
2024-07-05 09:25:00 +0545

    function scooping

---
+++ index.js
@@ -0,0 +1,727 @@
+//  Variables
+// var x = 90;
+// console.log(x)
+
+// var x = 500;
+// console.log(x)
+
+// let y = 90;
+// console.log(y)
+//  y =(900)
+// console.log(y)
+// const result = 9 + 9;
+// console.log(result)
+
+// result = 6-4;
+// console.log(result);
+
+// // Data Types
+// let x = 90;
+// console.log(x);
+// console.log(typeof x);
+
+//string
+// const result ="samuel";
+// console.log(result)
+// console.log(typeof a);
+// console.log(`who is gay? ${result}`);
+
+// undefined 
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// // null(object)
+// const y = null;
+// console.log(y);
+// console.log(typeof y)
+
+// //Boolean 
+// const isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue);
+
+// var result = 90;
+// console.log(result);
+
+
+// let x = 90;
+// console.log(x);
+
+// x = 100;
+// console.log(x);
+
+
+// const y = 80;
+// console.log(y);
+
+// y = 60;
+// console.log(y);
+
+// datatypes  in javascript
+//  Number
+//  const x = 90;
+//  console.log(x);
+//  console.log(typeof x);
+
+ // Boolean
+// const  isMarried  = false;
+// console.log(isMarried);
+// console.log(typeof isMarried);
+
+// // undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+
+// // null
+// const x = null;
+// console.log(x);
+// console.log(typeof a);
+
+//BigInt
+// const x = 54756558;
+// console.log(x);
+// console.log(typeof x);
+
+// // symbol
+//  const result = symbol("who are you?");
+//  console.log(result);
+//  console.log(typeof result);
+
+
+// object types
+
+// const obj = {
+//     name: "samuel",
+//     age: 20,
+//     isMarried: false,
+//     occupation: "Developers",
+//     salary:45000,
+// };
+// console.log(obj);
+// // console.log(typeof obj);
+
+// // Type conversion
+// const x = "123";
+// console.log(x);
+// const val = Number(x);
+// console,log(val);
+// console.log(typeof val);
+
+// const result = "8.98";
+// const output =  parseFloat(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result = 1+"1";
+// console.log(result);
+// console.log(typeof result);
+
+// const result = 8 - "4";
+// console.log(output);
+// console.log(typeof output);
+
+// const  x = "123";
+// const result = x.toString();
+// console.log(result);
+// console.log (typeof result);
+
+// const  x = "123";
+// const result = String(x);
+// console.log(result);
+// console.log (typeof result);
+
+// let result = true;
+// let output = string(result);
+// console.log(output);
+// console.log(typeof output);
+
+// let date = new date();
+// console.log(date);
+// console.log(typeof date);
+
+// let date = new Date();
+// let result = date.toString();
+// console.log(result);
+// console.log(typeof result);
+
+//  String Methods
+// const result = "Hello world"
+
+ // length
+// console.log(result.length);
+
+ // chartAt
+// console.log(result.chartAt(2));
+
+
+ // indexof
+// console.log(result.indexof("0"));
+
+ // LastIndexof
+// console.log(result.last("0"));
+
+ // lowercase
+// console.log(result.toLowerCase());
+
+ // uppercase
+// console.log(result.toUpperCase());
+
+ // split
+// console.log(result.split(""));
+
+// includes
+// console.log(result.includes("Worlds"));
+
+ // replace
+// console.log(result.replace("world","Everyone"));
+
+ // slice
+// console.log(result.slice(1,6));
+
+ // substring
+// console.log(result.substring(1,5));
+
+ // trim
+// const output = "samuel"
+// console.log(output.trim);
+
+ // concatenation
+// console.log(result.concat("i am js"));
+
+// operators
+// Assignment operaters
+
+// let y = 90;
+// y  += 120;
+// console.log(y);
+
+
+// comparison Operaters
+//  let a = 10;
+//  let b = 5;
+//  let c = "10"
+
+//  
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// let x = true;
+// let y = false;
+
+// console.log(x && y);
+// console.log(x || y);
+// console.log(!x);
+
+// const str1 = "Hello";
+// const str2 = "World";
+
+// console.log(str1 +" "+ str2);
+
+// conditional (Ternary) operator
+// let ageVote = 18;
+// let result = ageVote >= 18 ? "vote halna payeu": "payenau";
+// console.log(result);
+
+//To print something in console
+// console.log("Hello world!");
+// console.log('hello');
+
+//To show alert message pop up
+// window.alert('This is an alert');
+// window.alert('i like pizza');
+
+// To show text content for H1 element and p element from Html using js
+// document.getElementById("lol").textContent = 'Hello world!';
+// document.getElementById("paragraph").textContent = 'paragraph';
+
+
+
+
+
+
+//variables = A container that stores a value.
+//            Behaves as if it were the value it contains.
+// 1. declaration   let x;
+// 2. assignment    x=100;
+
+// let x;  (cannot declare two same variables)
+// let x; 
+
+// let x;
+// x=100;
+// console.log(x);   (shows the value of x in console: 100)
+// If you know the value of x, you can declare and assign at the same time: let x=100
+
+
+// let age= 25;           // all numerical datatype
+// let price = 10.99;
+// let gpa= 3.18;
+// console.log(price); (prints the value of price: 10.99)
+
+// console.log(typeof age);  // datatype
+//  console.log(`you are ${age} years old`); (prints as "you are 25 years old" in console)
+// console.log(`The price is $${price}`);
+//console.log(`your gpa is ${gpa} `);
+
+
+
+
+
+
+//  strings   (A string is a series of characters)
+// use "" or '' for strings
+
+// let firstName = "samuel";
+// let favoriteFood = "Pizza";
+// let email = "lol123@gmail.com";    (you can add numbers in string)
+
+// console.log(`my email is ${email}`);
+
+// console.log("favoriteFood");
+// console.log(`My favorite food is ${favoriteFood}`);
+
+// console.log(firstName);
+// console.log(typeof firstName);
+// console.log(`your name is ${firstName}`);
+
+
+
+
+
+
+//  booleans  (True or False #used as flags in your program)
+// let samuel= true;
+// let online= false;
+// let forSale= false;
+
+// console.log(`is this car for sale?  ${forSale}`);
+// console.log(typeof samuel);
+// console.log(`Samuel is online: ${samuel}`);
+// console.log(`Sandesh is online ${online}`);
+
+// const result = "hello world";
+// const output = "i love javascript";
+// const str3  = "lol";
+//  console.log(`${result} ${output} ${str3}` );
+
+//  console.log(`${result} ${output} ${str3}`);
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let ans = a+ b + c;
+// console.log(` the sum of three numbers are ${ans}`);
+
+//unary number
+// let ans = "234";
+// let output = +ans;
+// console.log(output);
+// console.log(typeof output)
+
+// let ans = "234";
+// let output = -ans;
+// console.log(output);
+// console.log(typeof output)'
+
+// IF else
+// let x = 85;
+// if(x >= 90)
+//     {
+//         console.log("Grade A+");
+//     }else if (x>=80){
+//         console.log("Grade A");
+//     }else if (x>= 70){
+//         console.log("Grade B+");
+//     }else if (x>= 60){
+//         console.log("Grade B");
+//     }else if (x>= 50){
+//         console.log("Grade C+");
+//     }
+//     else if (x>= 40){
+//         console.log("Grade c");
+//     }else {
+//         console.log("Failed");
+//     }
+
+    // let email = "samuel1gmail.com"
+    // let password = "234643645"
+
+    // if (email === "samuel1gmail.com" && password === "234643645") {
+    //     console.log("Login successful");
+    // } else {
+    //     console.log("Invalid Password");
+    // }
+
+    // condition statement
+    // let email = "samuel1gmail.com"
+    // let password = "234643645"
+
+    // if (email === "samuel1gmail.com" && password === "34643645") {
+    //     console.log("Login successful");
+    // } else {
+    //     console.log("Invalid Password");
+    // }
+
+    // let weather = "rainy";
+    // if (weather === "sunny"){
+    //       console.log("k aja Gham lagachata!")
+    // }else if(weather ==="Winter"){
+    //     console.log("Aja pani parnale chiso cha!")
+    // }else if(weather ==="cloudly"){
+    //     console.log("Badal Dhakamaka Lagekocha!")
+    // }else if(weather ==="rainy"){
+    //         console.log("chata bokera aayeko cha!")
+    //     }else{
+    //         console.log("Invalid weather")
+    //     }
+
+// let x = 5;
+// if(x >=5){
+//     console.log("x is greater than equal to s!");
+//     }else{
+//         console.log("x is not greater or equal to s !")
+//     }
+
+// Switch (control statement)
+// let role = "super admin";
+// switch(role) {
+//     case "seller":
+//         console.log("seller can add product");
+//         break;
+
+//         case "buyer":
+//         console.log("buyer can buy their products");
+//          break;
+
+//            case "admin":
+//             console.log("admin can access to add seller role");
+//             break;
+
+//               case "super admin":
+//                 console.log("super admin can control everything to be app");
+//                 break;
+
+//                    default:
+//                     console.log("Invalid role");
+//                     break;
+// }
+
+
+// let shipping = "Delivered"
+// switch (shipping) {
+//     case "processing":
+//     console.log("timro saman prcessing hudai cha");
+//         break;
+    
+//         case "shipped":
+//     console.log("Timro samn bato ma aaudai xa");
+//         break;
+    
+//         case "Delivered":
+//             console.log("location ma aaunusna saman leraye");
+//                 break;
+
+//     default:
+//         console.log("Invalid shipping process")
+//         break;
+// }
+
+ 
+
+// // statuscode
+// let statuscode = 200;
+// switch (statuscode){
+//     case 201:
+//         console.log("Created!");
+//         break;
+//         case 400:
+//         console.log("Bad Request!");
+//         break;
+//         case 401:
+//         console.log("UnAuthorized!");
+//         break;
+
+//         case 403:
+//         console.log("Forbidden!");
+//         break;
+
+//         case 500:
+//         console.log("Internal Server Error!");
+//         break;
+
+//         case 200:
+//         console.log("ok!");
+//         break;
+
+//         default:
+//             console.log("Invalid status code!");
+//             break;
+// }
+
+// loop
+// for loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`I love javascript: ${i}`);
+// }
+
+// for(let i =1; i<10; i++){
+//     console.log(`3*${i} = ${3*i}`);
+// }
+
+//  even number
+// for (let i = 1; i < 20; i++ ){
+//     if (i % 2 == 0){
+//         console.log(`The Even Number: ${i}`);
+//     }
+// }
+// odd number
+// for (let i = 1; i < 20; i++ ){
+//     if (i % 2  !== 0){
+//         console.log(`The odd Number: ${i}`);
+//     }
+// }
+  
+// (while loop)
+// let i =0;
+// while (i <= 10){
+//     console.log("Hello World" + i);
+//     i++;
+// }
+
+//(do while loop)
+// let i =1;
+// do {
+//     console.log("Hello world" + i);
+//     i++;
+    
+// } while (i <= 10);
+  
+
+// Array 
+// let result = [3,4,5,6,7,8];
+// let sum = 0;
+
+//  for(let i =  0; i < result.length; i ++ ) {
+//     sum += result[i];
+//  }
+//  console.log(`The sum of Arr is : ${sum}`);
+
+
+//  Array methods
+//  let arr = [4,5,6,7,8,2,3,4];
+
+//  length;
+// console.log(arr.length);
+
+// push
+// arr.push(12);
+// console.log(arr);
+  
+// pop
+// arr.pop(15);
+// console.log(arr);
+
+// shift
+// arr.shift(1,3);
+// console.log(arr);
+
+// unshift
+// arr.unshift(1,5);
+// console.log(arr);
+
+// splice
+// arr.splice(1,3);
+// console.log(arr);
+
+// let result = ["javascript","is","funny","language"];
+// console.log(result.join("-"));
+
+
+// slice
+// let result = arr.slice(1,4);
+// console.log(result);
+
+// includes
+// let result = arr.includes(4);
+// console.log(result);
+
+// let result = arr.indexOf(4);
+// console.log(result);
+
+// lastindexof
+// let result = arr.lastIndexOf(4);
+// console.log(result);
+
+// console.log(arr.concat("Hello"));
+
+// array value not put condition
+// let arr = [];
+// let sum = 0;
+
+// for (let i = 1; i<10; i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// for(let i = 0; i<arr.length; i++){
+//     sum += arr[i];
+// }
+// let result = sum *2;
+
+// console.log(`The sum of arr is: ${sum}`);
+// console.log(`The ans is: ${result}`);
+
+
+// object
+
+//  const person = {
+//    name: "Ram Thapa",
+//    age: 1,
+//    occupation: "Agriculture",
+//    isMarried: false,
+//    address:{
+//     street: "DilliBazar",
+//     city: "Kathmandu",
+//     country: "Nepal"
+//    },
+
+//    greet: function(){
+//     console.log(`Hello my name is:${this.name}.And I am : ${this.age}years old`
+
+//     );
+//    },
+//  };
+//  console.log(person.address.city);
+//  person.greet();
+
+
+
+
+//  Destructure
+
+// const{name,
+//     age,
+//     occupation,
+// isMarried,
+// address:{street, city, country},
+// } = person;
+
+// console.log(name, age, occupation, isMarried, street, city, country);
+// person.greet();
+
+
+// function
+// function myfunc(a,b){
+// return a + b;
+// }
+// let result = myfunc(8,9);
+// console.log(result);
+
+// function Circle(radius){
+//   return Math.PI * radius * radius;
+// }
+
+// let result = Circle(5);
+// console.log(`The area of circle is: ${result}`);
+
+
+// function Greet(name,age,education){
+// console.log(`Hello my name is:${name}. I am:${age} years old. And i have completed my bachelors in : ${education} from software college of IT & E-commerce`
+// );
+// }
+// Greet("Ram Thapa",20,"Bsc(hons)computing");
+
+
+// javascript ma three ways print message
+// console.log
+// alert
+// prompt
+
+
+// function myfunc() 1{
+//     let a = prompt("Enter your number");
+//     let b = prompt("Enter your number");
+//     let result = Number(a) + Number(b);
+//     if(result) {
+//         alert(`The sum of two numberis : ${result}`);
+//     }
+// }
+//  myfunc();
+
+
+// calculatingCartValue
+  
+// function CalculateTotal(cartItems,taxRate){
+//     let total = 0;
+// for (let i = 0; i < cartItems.length; i++) {
+//     total += cartItems[i].price*cartItems[i].quantity;
+// }
+// total += total *taxRate;
+// return total;
+// }
+// let cart = [
+//     {name:"Dr Martin",price:8000,quantity:1},
+//     {name:"Air Force 1",price:6000,quantity:2},
+// ];
+
+// let ans = CalculateTotal(cart, 0.02);
+// console.log(`Your full and final payment is: ${ans}`);
+
+// scoping
+// var x = 100;
+// let ans = 80;
+// const y = 20;
+
+// function myfunc(){
+//     console.log(x);
+//     console.log(y);
+//     console.log(ans);
+// }
+// myfunc();
+
+
+
+// function myfunc(){
+//     if (true) {
+//    var x = 90;
+//    let y = 80;
+//    const z = 60;
+   
+//    console.log(y);
+//    }
+//   console.log(x);
+// }
+// myfunc();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
